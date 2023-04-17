df8e07126de008efcc17f392ce914910e3ff9b69
DESKTOP-VC2DAST\bsnmj <bsnmjr@gmail.com>
2023-04-17 21:48:48 +0900

    https

--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,21 @@ import http from 'http';
 
 const app = new App().application;
 
-const httpServer = http.createServer( app );
+//const httpServer = http.createServer( app );
     
-//    const httpServer = http.createServer((req, res) => {
-//    res.writeHead(301, { Location: 'https://' + req.headers['host'] + req.url });
-//    res.end();
-//});
+const httpServer = http.createServer((req, res) => {
+    res.writeHead(301, { Location: 'https://' + req.headers['host'] + req.url });
+    res.end();
+});
 
-//const httpsServer = https.createServer(
-//    {
-//        key: fs.readFileSync('/root/.acme.sh/woongdo.kro.kr/woongdo.kro.kr.key', 'utf-8'),
-//        cert: fs.readFileSync('/root/.acme.sh/woongdo.kro.kr/woongdo.kro.kr.cer', 'utf-8'),
-//        ca: fs.readFileSync('/root/.acme.sh/woongdo.kro.kr/ca.cer', 'utf-8'),
-//    },
-//    app
-//);
+const httpsServer = https.createServer(
+    {
+        key: fs.readFileSync('/root/.acme.sh/woong-do.kro.kr/woong-do.kro.kr.key', 'utf-8'),
+        cert: fs.readFileSync('/root/.acme.sh/woong-do.kro.kr/woong-do.kro.kr.cer', 'utf-8'),
+        ca: fs.readFileSync('/root/.acme.sh/woong-do.kro.kr/ca.cer', 'utf-8'),
+    },
+    app
+);
 
 //app.listen( 80, ()=>{console.log(1)} );
 
@@ -29,6 +29,6 @@ httpServer.listen(80, () => {
     console.log('HTTP Server Start');
 });
 
-//httpsServer.listen(443, () => {
-//    console.log('HTTPS Server Start');
-//});
+httpsServer.listen(443, () => {
+    console.log('HTTPS Server Start');
+});
